3e1f2000522765de83d5ba1d97b62e985a754daf
charlesganne <charles.ganne@gmail.com>
2024-12-06 12:24:12 +0100

    insertions des modifications du sujet dans file_handler

--- src/file_handler.c
+++ src/file_handler.c
@@ -1,5 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
 #include <dirent.h>
 #include "file_handler.h"
+#include "deduplication.h"
+
+/* fonctions de la première version
+ * du sujet :
+ */
 
 char *list_files(const char *path) {
     struct dirent *dir;
@@ -120,4 +129,37 @@ void write_file(const char *filepath, const void *data, size_t size) {
     }
 
     fclose(file); // Fermer le fichier
+}
+
+/* fonctions de la dernière version
+ * du sujet
+ */
+
+// Fonction permettant de lire un élément du fichier .backup_log
+log_t read_backup_log(const char *logfile){
+    /* Implémenter la logique pour la lecture d'une ligne du fichier ".backup_log"
+    * @param: logfile - le chemin vers le fichier .backup_log
+    * @return: une structure log_t
+    */
+}
+
+// Fonction permettant de mettre à jour une ligne du fichier .backup_log
+void update_backup_log(const char *logfile, log_t *logs){
+  /* Implémenter la logique de modification d'une ligne du fichier ".bakcup_log"
+  * @param: logfile - le chemin vers le fichier .backup_log
+  *         logs - qui est la liste de toutes les lignes du fichier .backup_log sauvegardée dans une structure log_t
+  */
+
+}
+
+void write_log_element(log_element *elt, FILE *logfile){
+  /* Implémenter la logique pour écrire un élément log de la liste chaînée log_element dans le fichier .backup_log
+   * @param: elt - un élément log à écrire sur une ligne
+   *         logfile - le chemin du fichier .backup_log
+   */
+}
+
+void list_files(const char *path){
+  /* Implémenter la logique pour lister les fichiers présents dans un répertoire
+  */
 }
--- src/file_handler.h
+++ src/file_handler.h
@@ -1,14 +1,41 @@
+#ifndef FILE_HANDLER_H
+#define FILE_HANDLER_H
+
 #include <stddef.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <openssl/md5.h>
 
-#ifndef FILE_HANDLER_H
-#define FILE_HANDLER_H
-
+// fonctions de l'ancienne version du sujet
 
 char *list_files(const char *path);
 char *read_file(const char *filepath, size_t *size);
 void write_file(const char *filepath, const void *data, size_t size);
+
+// types du nouveau sujet
+
+// Structure pour une ligne du fichier log
+typedef struct log_element{
+    const char *path; // Chemin du fichier/dossier
+    unsigned char md5[MD5_DIGEST_LENGTH]; // MD5 du fichier dédupliqué
+    char *date; // Date de dernière modification
+    struct log_element *next;
+    struct log_element *prev;
+} log_element;
+
+// Structure pour une liste de log représentant le contenu du fichier backup_log
+typedef struct {
+    log_element *head; // Début de la liste de log 
+    log_element *tail; // Fin de la liste de log
+} log_t;
+
+// fonctions de la nouvelle version du sujet
+
+log_t read_backup_log(const char *logfile);
+void update_backup_log(const char *logfile, log_t *logs);
+void write_log_element(log_element *elt, FILE *logfile);
+void list_files(const char *path);
+void copy_file(const char *src, const char *dest);
 
 #endif // FILE_HANDLER_H
