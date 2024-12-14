9b3b78cb66065a9de1d3b40e61f480d41be6057d
Julie BENED <116278302+julies01@users.noreply.github.com>
2024-12-14 11:59:34 +0100

    remise normale de la branche

--- src/network.h
+++
@@ -1,9 +0,0 @@
-#include <stddef.h>
-
-#ifndef NETWORK_H
-#define NETWORK_H
-
-void send_data(const char *server_address, int port, const void *data, size_t size);
-void receive_data(int port, void **data, size_t *size);
-
-#endif // NETWORK_H
